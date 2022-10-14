b9081f124e0422fcafbfd71e83252fb9f3dd466d
jennanjohnson <john1627@miamioh.edu>
2022-10-14 10:58:19 -0400

    Added the code with the plot random unction

---
+++ plot_random_function.R
@@ -0,0 +1,48 @@
+############################
+## plot_random_function.R
+##
+## Jenna Johnson
+##
+## Purpose: a function that will generate a random
+##     sample of normal values, plot a histogram
+##     with an overlaid mean on the plot
+##     and returns summary stats about the generated data. 
+## 
+## Inputs: numpts = the number of points to generate, defaults to 50
+##         bins = the number of break points in the hist() function, 
+##                defaults to 10
+##         mu = mean of normal distribution, defaults to 0
+##         sigma = standard deviation of normal distribution, defaults to 1
+##         seed = seed for the random number generator, defaults to NULL --
+##                so random each time, but user can override the value
+##
+## Outputs:
+##   A list is returned with the following elements
+##        Random_sample = the generated random sample
+##        Mean_sample = the mean of the random sample
+##        SD_sample = the standard deviation of the random sample
+
+plotRandomNormals <- function(numpts=50, bins=10, 
+                              mu=0, sigma=1, 
+                              seed=NULL) {
+  set.seed(seed)
+  rand_sample <- rnorm(numpts, mean=mu, sd=sigma)
+  hist(rnorm(numpts, mean=mu, sd=sigma), breaks=bins+1)
+  abline(v=mean(rand_sample), col="blue" )
+  list(Random_sample= rand_sample, 
+       Mean_sample = mean(rand_sample), 
+       SD_sample= sd(rand_sample) )
+}
+
+## Default values
+plotRandomNormals()
+
+## User specified number of points
+plotRandomNormals(numpts=30)
+
+plotRandomNormals(numpts=1000, bins=200)
+
+plotRandomNormals(numpts=1000, bins=50, mu=63.5, sigma=4.5)
+
+plotRandomNormals(seed=1)
+
